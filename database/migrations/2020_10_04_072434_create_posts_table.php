41dcad4fcae683d3e0042add0a1193cdd449e100
Linchaker <linchaker@gmail.com>
2020-10-04 10:39:40 +0300

    create migrations

---
+++ database/migrations/2020_10_04_072434_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('author_id');
+            $table->unsignedInteger('image_id')->nullable();
+            $table->text('content');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('author_id')->references('id')->on('users');
+            $table->foreign('image_id')->references('id')->on('images');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_author_id_foreign');
+            $table->dropForeign('posts_image_id_foreign');
+        });
+
+        Schema::dropIfExists('posts');
+    }
+}
