8e481ae29143f6ed5cea0e3f96cc0567c0d9acc7
Linchaker <linchaker@gmail.com>
2020-10-04 15:58:27 +0300

    add UserController add posts_limit to query

---
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $limit = (int) $request->get('posts_limit', 100);
+
+        $data = User::where('active', true)->get();
+
+        if ($limit > 0) {
+            $data = $data->map(function($feed) use ($limit) {
+                $feed->setRelation('posts', $feed->posts->take($limit));
+                return $feed;
+            });
+        }
+
+        return new \App\Http\Resources\UserCollection($data);
+    }
+}
